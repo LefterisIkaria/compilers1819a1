d05b043d3dd301664992d99897db7ba40fda3e1a
LefterisIkaria <32582043+LefterisIkaria@users.noreply.github.com>
2019-03-03 18:44:48 +0200

    initial commit

--- scanner.py
+++ scanner.py
@@ -8,19 +8,23 @@ NOTE: suitable for optional matches
 def getchar(text,pos):
 	""" returns char category at position `pos` of `text`,
 	or None if out of bounds """
-	
+
 	if pos<0 or pos>=len(text): return None
-	
+
 	c = text[pos]
-	
+
 	# **Σημείο #3**: Προαιρετικά, προσθέστε τις δικές σας ομαδοποιήσεις
-	
-	if c>='0' and c<='9': return 'DIGIT'	# 0..9 grouped together
-	
-	if c=='.': return 'DOT'	# dot as a category by itself
-	
+	if pos = 0:
+			if c>='0' and c<='2': return '0-2'	# 0..9 grouped together
+
+	if pos = 1 and c = '3':
+		if c>='0' and c<='4': return '0-4'
+
+	if (pos = 1 and c = '0-2') or (pos = 2 and (c = '0-4' or c '0-9')) or pos >= 3:
+		if c>='0' and c<='9': return '0-9'
+
 	return c	# anything else
-	
+
 
 
 def scan(text,transitions,accepts):
@@ -28,48 +32,55 @@ def scan(text,transitions,accepts):
 	'transitions'. After that, if in a state belonging to
 	`accepts`, it returns the corresponding token, else ERROR_TOKEN.
 	"""
-	
+
 	# initial state
 	pos = 0
-	state = 's0'
+	state = 'q0'
 	# memory for last seen accepting states
 	last_token = None
 	last_pos = None
-	
-	
+
+
 	while True:
-		
+
 		c = getchar(text,pos)	# get next char (category)
-		
+
 		if state in transitions and c in transitions[state]:
 			state = transitions[state][c]	# set new state
 			pos += 1	# advance to next char
-			
+
 			# remember if current state is accepting
 			if state in accepts:
 				last_token = accepts[state]
 				last_pos = pos
-			
+
 		else:	# no transition found
 
 			if last_token is not None:	# if an accepting state already met
 				return last_token,last_pos
-			
+
 			# else, no accepting state met yet
 			return 'ERROR_TOKEN',pos
-			
-	
+
+
 # **Σημείο #1**: Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων
-transitions = { 's0': { 'DIGIT':'s1' },
-       			's1': { 'DIGIT':'s1','DOT':'s2' },
-       			's2': { 'DIGIT':'s3' },
-       			's3': { 'DIGIT':'s3' }       
-     		  } 
+transitions = { 'q0': { '0-2':'q1', '3':'q2' },
+       			'q1': { '0-9':'q3' },
+       			'q2': { '0-4':'q4', '5':'q5' },
+       			'q3': { '0-9':'q6' },
+				'q4': { '0-9':'q6' },
+				'q5': { '0':'q6' },
+				'q6': { '0-9':'q7' },
+				'q7': { '0-9':'q8' },
+				'q8': { 'K':'q9', 'G':'q10' },
+				'q9': { 'T':'q13' },
+				'q10': { '0-9':'q11' },
+				'q11': { '0-9':'q12' },
+				'q12': { 'K':'q9' },
+     		  }
 
 # **Σημείο #2**: Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής
-accepts = { 's1':'INT_TOKEN',
-       		's3':'FLOAT_TOKEN'	
-     	  }
+accepts = { 'q13':'WIND_TOKEN' }
 
 
 # get a string from input
@@ -85,4 +96,3 @@ while text:		# i.e. len(text)>0
 	print("token:",token,"text:",text[:pos])
 	# new text for next scan
 	text = text[pos:]
-	
